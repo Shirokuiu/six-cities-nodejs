92feff4803529a0a9bd31b235cdb0d13a73b50f5
pavel <shirokuiuna@mail.ru>
2025-03-24 11:42:55 +0300

    Добавил CLIApplication, точка входа для команд cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,3 +2,4 @@ export * from 'src/cli/commands/command.interface.js';
 export * from 'src/cli/commands/types.js';
 export * from 'src/cli/commands/version.command.js';
 export * from 'src/cli/commands/help.command.js';
+export * from './cli-application.js';
