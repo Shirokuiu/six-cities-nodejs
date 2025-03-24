0e4e20dc852e48dffde6d1f00dfed94a643708d2
pavel <shirokuiuna@mail.ru>
2025-03-24 11:47:30 +0300

    Доработка по исполняемым командам

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,12 @@
 import { Command } from './commands/command.interface.js';
+import { CommandParser } from 'src/cli/command-parser.js';
 
 type CommandCollection = Record<string, Command>;
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(private readonly defaultCommand: string = '--help') {}
 
   registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -13,5 +16,26 @@ export class CLIApplication {
 
       this.commands[command.getName()] = command;
     });
+  }
+
+  getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+
+    return this.commands[this.defaultCommand];
+  }
+
+  processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+
+    command.execute(...commandArguments);
   }
 }
